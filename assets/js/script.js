90788aaf65ed208c0efdfda6b62ab1ae1d605992
Olive <olivelliott48@gmail.com>
2022-05-28 21:30:23 -0400

    Create handle new search feature and push userEntry to fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+var submitButtonEl = $('#submit-btn');
+var account = 'ruNaWJgAnvmlDC7luYrIu0wwVIofUPwcmWpHkPJZ'
+
+//  *2
+//  ID's needed: search form's input ID
+function handleNewSearch (event) {
+    event.preventDefault();
+    userEntry = $(this).siblings('#placeholder').val().trim();
+    console.log(userEntry)
+    if (userEntry) {
+        fetchResultsList(userEntry);
+        $('#placeholder').val('');
+    }
+}
+
+//  * 3
 // Autocomplete API to get an array of titles matching the search
 var fetchResultsList = function (userEntry) {
 	let url = `https://api.watchmode.com/v1/autocomplete-search/?apiKey=${account}&search_value=${userEntry}`;
@@ -17,3 +33,6 @@ var findTitleSources = function (choosenTitleId) {
 var getTitleReviews = function (imdbId) {
 	let url = `https://imdb-api.com/en/API/Ratings/${imdbApiKey}/${imdbId}`;
 };
+
+
+$(submitButtonEl).on('click', handleNewSearch);
