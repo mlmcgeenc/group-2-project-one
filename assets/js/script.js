ab5dfa4ee0fc9559c7292fd06aa1e1c9c7d509bb
Matt McGee <mlmcgeenc@gmail.com>
2022-05-29 19:55:39 -0400

    connect all JS, refactor some code, meeting end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
-const searchResultsEl = document.getElementById('placeholder-results-div');
+const searchResultsEl = document.getElementById('results-list');
 const contentEl = document.getElementById('content');
 const mattsWatchmodeAccount = 'CFUyQoNYEjDUNjdIGVUjd03eAKPBvYKRtCQAdiUu';
 const mattsImdbAccount = 'k_h9vj9ndq';
 let account = '7JHhPYyxv2UtIUfImk4BYOdeKZvgQ6r1Wba971Cv';
-let userEntry = 'Goodfellas';
 let imdbApiKey = 'k_v4xuex9v';
-var submitButtonEl = $('#submit-btn');
+// var submitButtonEl = $('#search-results');
+var submitButtonEl = document.getElementById('search-results');
 let searchDiv = $('#search-div');
 let recentSearchDiv = document.querySelector('#recent-search-div');
 let recents_ul = $('#recents-list');
 let searchTextInput, currentRqst;
 let isGoodRequest = true;
+var formEl = document.getElementById('search-form');
 
 // Declare an object named selectionInfo that will contain the needed info collected through API calls
 let selectionInfo = {
@@ -20,19 +21,29 @@ let selectionInfo = {
 	streaming_services: [],
 };
 
-
 // ==================== SEARCH ====================
 //  Push the user entry input to fetchResultsList
 //  * ID's needed: search form's input ID
 function handleNewSearch(event) {
 	event.preventDefault();
-	userEntry = $(this).siblings('#placeholder').val().trim();
+	searchResultsEl.innerHTML = '';
+	userEntry = $(this).siblings('#searchTxt').val().trim();
 	if (userEntry) {
 		fetchResultsList(userEntry);
-		$('#placeholder').val('');
+		$('#searchTxt').val('');
 	}
 	//  ? should we add an else statement with a modal 'Please enter a search item'
 }
+
+// Autocomplete API to get an array of titles matching the search
+const fetchResultsList = function (userEntry) {
+	let url = `https://api.watchmode.com/v1/autocomplete-search/?apiKey=${mattsWatchmodeAccount}&search_value=${userEntry}`;
+	fetch(url)
+		.then((res) => res.json())
+		.then((json) => {
+			buildResultsList(json);
+		});
+};
 
 // Take user entry data and build a list of results and dynamically display onto the DOM
 //  * ID's needed: div container for displaying the results, id for styling the displayed results
@@ -43,21 +54,11 @@ function buildResultsList(json) {
 		var typeOfShow = json.results[i].type.replace('_', ' ');
 		var dataId = json.results[i].id;
 
-		var resultButton = $(`<button class='results-list-styling-placeholder' data-id=${dataId}></button>`).appendTo('#placeholder-results-div');
+		var resultButton = $(`<button class='results-list-styling-placeholder' data-id=${dataId}></button>`).appendTo('#results-list');
 		// var displayResults = $(`<ul class='results-list-styling-placeholder'></ul>`).appendTo(resultButton);
 		$(`<li class='results-list-styling-placeholder'> ${nameTitle} ${releaseTitle} ${typeOfShow} </li>`).appendTo(resultButton);
 	}
 }
-
-// Autocomplete API to get an array of titles matching the search
-const fetchResultsList = function (inputValue) {
-	let url = `https://api.watchmode.com/v1/autocomplete-search/?apiKey=${mattsWatchmodeAccount}&search_value=${inputValue}`;
-	fetch(url)
-		.then((res) => res.json())
-		.then((json) => {
-			buildResultsList(json);
-		});
-};
 
 // ==================== USER SELECTS A RESULT FROM LIST RETURNED FROM SEARCH ====================
 const handleMakeSelection = function (e) {
@@ -134,15 +135,15 @@ let upDateStorage = function (item) {
 };
 
 /* incorporate to Matt's eventhandler */
-$('#search-results').click(function () {
-	searchTextInput = document.querySelector('#searchTxt');
-	currentRqst = searchTextInput.value;
-	if (isGoodRequest) upDateStorage(currentRqst);
-	searchTextInput.value = '';
-	/* get Matt to return an isGoodRequest var
-	 so we confirm b/4 adding to storage.
-	 Hardcoded for now. */
-});
+// $('#search-results').click(function () {
+// 	searchTextInput = document.querySelector('#searchTxt');
+// 	currentRqst = searchTextInput.value;
+// 	if (isGoodRequest) upDateStorage(currentRqst);
+// 	searchTextInput.value = '';
+// 	/* get Matt to return an isGoodRequest var
+// 	 so we confirm b/4 adding to storage.
+// 	 Hardcoded for now. */
+// });
 
 /* element is set to hidden via CSS */
 let manage_element_visi = function (element_list, doHide) {
@@ -158,4 +159,6 @@ let manage_element_visi = function (element_list, doHide) {
 };
 
 searchResultsEl.addEventListener('click', handleMakeSelection);
-$(submitButtonEl).on('click', handleNewSearch);
+// $(submitButtonEl).on('click', handleNewSearch);
+submitButtonEl.addEventListener('click', handleNewSearch);
+// formEl.addEventListener('submit', handleNewSearch);
