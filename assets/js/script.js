08cf2fd25498c15c77d427ac42adeb65e1479190
Olive <olivelliott48@gmail.com>
2022-05-28 22:27:10 -0400

    Create buildResultsList function and display results in ordered list appended to the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,9 +27,33 @@ var fetchResultsList = function (userEntry) {
 };
 
 // Take user entry data and build a list of results and dynamically display onto the DOM
+//  * ID's needed: div container for displaying the results, id for styling the displayed results
 function buildResultsList(json) {
-    console.log(json);
-}
+    var displayResults = $('<ul>').addClass('results-list').appendTo('#placeholder-results-div');
+
+    for (var i = 0; i < json.results.length; i++) {
+    var nameTitle = json.results[i].name;
+    var releaseTitle = json.results[i].year
+    var typeOfShow = json.results[i].type
+
+
+    $('<li>' + nameTitle + '</li>').addClass('results-styling-placeholder').appendTo(displayResults);
+    $('<li>' + releaseTitle + '</li>').addClass('results-styling-placeholder').appendTo(displayResults);
+    $('<li>' + typeOfShow + '</li>').addClass('results-styling-placeholder').appendTo(displayResults);
+    $('</br>').appendTo(displayResults)
+    }
+};
+// results: Array(49)
+// 0:
+// id: 3173903
+// image_url: "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"
+// name: "Breaking Bad"
+// relevance: 247.33
+// result_type: "title"
+// tmdb_id: 1396
+// tmdb_type: "tv"
+// type: "tv_series"
+// year: 2008
 
 // Title details API call provides ImDB number and sources array
 var findTitleSources = function (choosenTitleId) {
