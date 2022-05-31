35a14e07364fb3d7dd32f5771eef4c590bbfd20e
Matt McGee <mlmcgeenc@gmail.com>
2022-05-31 18:01:31 -0500

    refactored switch statement

--- assets/js/results-page-script.js
+++ assets/js/results-page-script.js
@@ -17,74 +17,58 @@ const buildResultsContent = function () {
 	const sourcesList = document.createElement('ul');
 	selectionInfo.streaming_services.forEach(function (entry) {
 		const sourceIcon = document.createElement('img');
+		sourceIcon.setAttribute('class', 'source-icon')
 		switch (entry.sourceId) {
 			case 24:
 				sourceIcon.setAttribute('src', './assets/images/amazon-prime.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 26:
 				sourceIcon.setAttribute('src', './assets/images/amazon-prime.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 371:
 				sourceIcon.setAttribute('src', './assets/images/apple-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 443:
 				sourceIcon.setAttribute('src', './assets/images/spectrum-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 442:
 				sourceIcon.setAttribute('src', './assets/images/direct-tv-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 372:
 				sourceIcon.setAttribute('src', './assets/images/disney-plus-logo.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 373:
 				sourceIcon.setAttribute('src', './assets/images/fubo-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 140:
 				sourceIcon.setAttribute('src', './assets/images/google-play-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 387:
 				sourceIcon.setAttribute('src', './assets/images/hbo-max-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 157:
 				sourceIcon.setAttribute('src', './assets/images/hulu-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 349:
 				sourceIcon.setAttribute('src', './assets/images/itunes-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 398:
 				sourceIcon.setAttribute('src', './assets/images/microsoft-store-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 203:
 				sourceIcon.setAttribute('src', './assets/images/netflix-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 272:
 				sourceIcon.setAttribute('src', './assets/images/tbs-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 307:
 				sourceIcon.setAttribute('src', './assets/images/vudu-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			case 344:
 				sourceIcon.setAttribute('src', './assets/images/youtube-icon.png')
-				sourceIcon.setAttribute('class', 'source-icon')
 				break;
 			default:
 				sourceIcon.setAttribute('src', './assets/images/wifi-solid.svg')
-				sourceIcon.setAttribute('class', 'source-icon')
 		}
 		const listItem = document.createElement('li');
 		const sourceName = document.createElement('h3');
